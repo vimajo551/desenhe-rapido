0bc6e87b0877ddd01c63596835135116213ad6da
vimajo551 <104533881+vimajo551@users.noreply.github.com>
2023-01-07 18:16:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+randomNumber = [Math.floor(Math.random() * 345) + 1, quickDrawDataset ];
+console.log(randomNumber,quickDrawDataset)
+Sketch = [randomNumber = quickDrawDataset]
+ename = Sketch
+
+timerCounter = 0;//inicializa esta variável para 0. Esta variável continuará incrementando o valor do contador do timer.
+timerCheck = 0;//adiciona uma string vazia dentro dela. Isso continuará verificando o valor do temporizador.
+drawSketch = 0;//adiciona uma string vazia dentro dela. Isso mantém a estimativa aproximada para o esboço que você desenhou na tela.
+answerHolder = 0;//adiciona uma string vazia dentro dela. Esta variável mantém o valor quando o esboço desenhado e o esboço a ser desenhado coincidem.
+score = 0; 
+//inicializa esta variável com 0. Esta variável mantém o valor da pontuação para cada esboço à direita desenhado.
